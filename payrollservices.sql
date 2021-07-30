493167fa09611e73d5ee0e82526d2033516acc5c
MOHAMEDAfrath <afrathismath@gmail.com>
2021-07-30 11:46:57 +0530

    [Afrath]Updated the file with create table and column query

--- payrollservices.sql
+++ payrollservices.sql
@@ -1,3 +1,10 @@
 -----UC-1 Create DataBase-----
 create database payroll_service
 use payroll_service
+-----UC-2 Create table for DataBase payroll-----
+create table employee_payroll(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+EmployeeSalary float,
+StartDate Date
+)
