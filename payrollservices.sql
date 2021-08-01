a340aa9d68347a4de66c4590d79c2f29d3380837
MOHAMEDAfrath <afrathismath@gmail.com>
2021-08-01 14:43:50 +0530

    [Afrath]Updated the file with create new table employeeDept to break m-m relationship

--- payrollservices.sql
+++ payrollservices.sql
@@ -48,8 +48,69 @@ Update employee_payroll set TaxablePay = '1000'
 Update employee_payroll set IncomeTax = '200'
 -----UC-10 Add Priya to Marketing department -----
 Insert into employee_payroll(EmployeeName,BasicPay,StartDate,Gender,EmployeePhoneNumber,EmployeeDepartment,EmployeeAddress,Deductions,TaxablePay,IncomeTax,NetPay)values('Priya','350500','2021-01-17','F','7345787969','Marketing','Chennai','0','0','0','0');
-
-
-
-
+-----UC-11 Create table Empt Dept-----
+---Company Details---
+create table company
+(
+company_Id int identity(1,1) primary key,
+company_name varchar(255)
+)
+Insert into company values('Xoxo'),('Vohi')
+select * from company
+---Employee Details---
+create table Employee
+(
+EmployeeId int identity(1,1) primary key,
+EmployeeName varchar(255),
+Gender char(1),
+EmployeePhoneNumber bigint,
+EmployeeAddress varchar(255),
+StartDate date,
+CompanyId int
+Foreign key(CompanyId) references Company(company_Id)
+)
+Insert into Employee values
+('Ram Kumar','M','7812453698','Chennai','2020-10-07','1'),
+('Vijay','M','7214587875','Banglore','2019-05-08','2'),
+('Priya','F','7345787969','Chennai','2021-01-17','2'),
+('Asif','M','9814753647','Mysore','2017-12-12','1')
+select * from Employee
+---Payroll table---
+create table payroll
+(
+EmpId int, 
+BasicPay float,
+TaxablePay float,
+IncomeTax float,
+Deductions float,
+NetPay float
+foreign key(EmpId) references Employee(EmployeeId)
+)
+Insert into payroll(EmpId,BasicPay,IncomeTax,Deductions)values('1','650000','20000','24000'),
+('2','700000','20000','23000'),
+('3','350500','20000','20000'),
+('4','950500','23000','24000')
+select * from payroll
+Update payroll set TaxablePay = (BasicPay-Deductions)
+Update payroll set NetPay = (TaxablePay-IncomeTax)
+---Dept Table---
+create table department_table
+(
+DeptId int identity(1,1) primary key,
+DeptName varchar(255)
+)
+insert into department_table values('Sales'),('Marketing'),('HR'),('Customer Service')
+select * from department_table
+-----Emp Dept table-----
+create table emp_Dept
+(
+DeptId int,
+EmpId int
+foreign key(EmpId) references Employee(EmployeeId),
+foreign key(DeptId) references department_table(DeptId)
+)
+insert into emp_Dept values
+('3','1'),('1','2'),
+('4','3'),('3','4'),('2','3')
+select * from emp_Dept
 
